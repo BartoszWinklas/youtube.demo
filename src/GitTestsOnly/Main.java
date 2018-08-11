62d9296d3829a2ce8f094fb1b7eaf8a2ceadc1a8
Lix <winklas.bartosz@gmail.com>
2018-08-11 15:20:32 +0200

    more changest to Main

--- src/GitTestsOnly/Main.java
+++ src/GitTestsOnly/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(" This is only a test to the github!!! :) ");
+
+
+        /*
+        Coding further and further, so we need to upgrade the repository at the github :))
+         */
     }
 }
